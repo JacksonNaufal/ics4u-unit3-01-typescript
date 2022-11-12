49c05e582ef5fdbfec5b47f78829a10a0afff69e
Jackson-Naufal <Jackson.naufal@mths.ca>
2022-11-12 17:22:11 +0000

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,39 @@
 /**
  * The program is the classic
- * Vehicle stats program
+ * Recurrsion rearange program
  *
  * By:      Jackson Naufal
  * Version: 1.0
- * Since:   2020-10-17
+ * Since:   2020-11-08
  */
 
-import Truck from './Truck'
-import Bike from './Bike'
+import promptSync from 'prompt-sync'
 
-const bmx = new Bike('Red', 40)
+/**
+ *
+ * This is the recurrsion function.
+ *
+ * @param {string} userWord the user input.
+ * @returns {string} the string thats reversed.
+ */
+function recurrsionString(userWord: string): string {
+  if (userWord === '') {
+    return userWord
+  } else {
+    return recurrsionString(userWord.slice(1)) + userWord.slice(0, 1)
+  }
+}
 
-console.log('Created Bmx bike.\nStatus:')
-bmx.status()
+const prompt = promptSync()
 
-console.log('Set the cadense to 10')
-bmx.accelerate(10)
-bmx.status()
+// Asks for the users input
+console.log('This program reverses a String!')
+const userWord = prompt('Enter your word: ')
 
-console.log('\nAccelerate by 15:')
-bmx.accelerate(15)
-bmx.status()
+// Shows the original word
+console.log(`This is the Original Word: ${userWord}`)
 
-console.log('Ring bell.')
-bmx.ringBell()
-
-const bigTruck = new Truck('Grey', 'HGC-3456F', 200, 5)
-
-console.log('\nCreated a Truck.\nStatus:')
-bigTruck.status()
-
-console.log('\nAccelerating, 10 of power for ten seconds:')
-bigTruck.accelerate(10, 10)
-console.log('New speed: ' + String(bigTruck.getSpeed()))
-
-console.log('\nBreaking, 10 of power for 10 sec.')
-bigTruck.accelerate(10, 10)
-console.log('New speed: ' + String(bigTruck.getSpeed()))
-
-console.log('\nApplyed air pressure of 5:')
-bigTruck.break(5, 5)
-console.log('New speed: ' + String(bigTruck.getSpeed()))
+// Shows the reversed string word.
+console.log(`The reversed word is: ${recurrsionString(userWord)}`)
 
 console.log('\nDone.')
